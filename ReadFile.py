742a8c4ddc974e688e6190bcfb963645dea01717
GuHan99 <GU_HAN@guangs-mbp.wireless.concordia.ca>
2018-04-01 15:21:56 -0400

    aaa

--- ReadFile.py
+++ ReadFile.py
@@ -1,18 +1,33 @@
 from pyspark.ml.fpm import FPGrowth
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+
+
+def merge(*c):
+    merged = sorted(set(c))
+    if len(merged) == 1:
+        return merged[0]
+    else:
+        return "[{0}]".format(",".join(merged))
+
 
 spark = SparkSession.builder.appName('data').getOrCreate()
 df = spark.read.csv('games.csv', header=True)
 
-fpGrowth = FPGrowth(itemsCol="items", minSupport=0.5, minConfidence=0.6)
-model = fpGrowth.fit(df)
+merge_udf = udf(merge)
 
-# Display frequent itemsets.
-model.freqItemsets.show()
+df = df.select(merge_udf("t1_champ1id", "t1_champ2id", "t1_champ3id", "t1_champ4id", "t1_champ5id"))
 
-# Display generated association rules.
-model.associationRules.show()
-
-# transform examines the input items against all the association rules and summarize the
-# consequents as prediction
-model.transform(df).show()
+df.show()
+# fpGrowth = FPGrowth(itemsCol="items", minSupport=0.5, minConfidence=0.6)
+# model = fpGrowth.fit(df)
+#
+# # Display frequent itemsets.
+# model.freqItemsets.show()
+#
+# # Display generated association rules.
+# model.associationRules.show()
+#
+# # transform examines the input items against all the association rules and summarize the
+# # consequents as prediction
+# model.transform(df).show()
