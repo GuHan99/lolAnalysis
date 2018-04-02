f25a885a424e01f927dba9ded79a1a110bed8d99
GuHan99 <GU_HAN@guangs-MacBook-Pro.local>
2018-04-02 11:46:21 -0400

    aa

--- frequent_enemy.py
+++ frequent_enemy.py
@@ -37,7 +37,7 @@ df = model.freqItemsets
 
 df = df.withColumn('length', size(df.items))
 df = df.filter(df.length == 2)
-df = df.filter(df.freq > 1000)
+df = df.filter(df.freq > 500)
 df = df.orderBy(df.length.asc(), df.freq.desc()).select('items', 'freq').collect()
 for i in df:
     print(i)
@@ -46,4 +46,7 @@ rules = model.associationRules
 
 rules = rules.withColumn('length', size(rules.antecedent))
 rules = rules.orderBy(rules.length.desc(), rules.confidence.desc()).select('antecedent', 'consequent', 'confidence')
-rules.show()
+rules = rules.collect()
+
+for i in rules:
+    print(i)
