8e1ed5193ffc2d3dc7c4b1332be5fc0bbb103cd3
GuHan99 <GU_HAN@guangs-MacBook-Pro.local>
2018-04-03 10:11:19 -0400

    aa

--- naive_bayes.py
+++ naive_bayes.py
@@ -10,31 +10,28 @@ maturity_udf = udf(lambda x: x, IntegerType())
 
 
 spark = SparkSession.builder.master('local').appName('data').getOrCreate()
-data_o = spark.read.csv('games.csv', header=True)
+data = spark.read.csv('games.csv', header=True)
 
-data = data_o.select(
-    data_o['winner'].cast(IntegerType()), data_o['firstBlood'].cast(IntegerType())
-    , data_o['firstTower'].cast(IntegerType())
-    , data_o['firstInhibitor'].cast(IntegerType()), data_o['firstBaron'].cast(IntegerType())
-    , data_o['firstDragon'].cast(IntegerType())
-    , data_o['firstRiftHerald'].cast(IntegerType()))
-data = data.withColumn('towerkill', data_o.t1_towerKills-data_o.t2_towerKills)
-data = data.withColumn('inhibitorkill', maturity_udf(data_o['t1_inhibitorKills']-data_o['t2_inhibitorKills']))
-data = data.withColumn('baronkill', maturity_udf(data_o['t1_baronKills']-data_o['t2_baronKills']))
-data = data.withColumn('dragonkill', maturity_udf(data_o['t1_dragonKills']-data_o['t2_dragonKills']))
-data = data.withColumn('riftkill', maturity_udf(data_o['t1_riftHeraldKills']-data_o['t2_riftHeraldKills']))
+data = data.withColumn('towerkill', data.t1_towerKills-data.t2_towerKills)
+data = data.withColumn('inhibitorkill', maturity_udf(data['t1_inhibitorKills']-data['t2_inhibitorKills']))
+data = data.withColumn('baronkill', maturity_udf(data['t1_baronKills']-data['t2_baronKills']))
+data = data.withColumn('dragonkill', maturity_udf(data['t1_dragonKills']-data['t2_dragonKills']))
+data = data.withColumn('riftkill', maturity_udf(data['t1_riftHeraldKills']-data['t2_riftHeraldKills']))
+
 data = data.select(
-    data['winner'], data['firstBlood']
-    , data['firstTower']
-    , data['firstInhibitor'], data['firstBaron']
-    , data['firstDragon']
-    , data['firstRiftHerald']
-    , data['towerkill']
-    , data['inhibitorkill']
-    , data['baronkill']
-    , data['dragonkill']
-    , data['riftkill']
+    data['winner'].cast(IntegerType()), data['firstBlood'].cast(IntegerType())
+    , data['firstTower'].cast(IntegerType())
+    , data['firstInhibitor'].cast(IntegerType()), data['firstBaron'].cast(IntegerType())
+    , data['firstDragon'].cast(IntegerType())
+    , data['firstRiftHerald'].cast(IntegerType())
+    , data['towerkill'].cast(IntegerType())
+    , data['inhibitorkill'].cast(IntegerType())
+    , data['baronkill'].cast(IntegerType())
+    , data['dragonkill'].cast(IntegerType())
+    , data['riftkill'].cast(IntegerType())
+
 )
+
 
 assembler = VectorAssembler(
     inputCols=['firstBlood', 'firstTower', 'firstInhibitor', 'firstBaron', 'firstDragon',
