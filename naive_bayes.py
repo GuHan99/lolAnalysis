2135b177281cce48187221161c40c56d9df279b6
GuHan99 <GU_HAN@guangs-MacBook-Pro.local>
2018-04-02 23:52:47 -0400

    aaa

--- naive_bayes.py
+++ naive_bayes.py
@@ -32,9 +32,7 @@ data_rdd = data_rdd.map(lambda x: Row(label=x[0], features=Vectors.dense([x[1], 
 data = spark.createDataFrame(data_rdd)
 
 # Split the data into train and test
-splits = data.randomSplit([0.6, 0.4], 123)
-train = splits[0]
-test = splits[1]
+(train, test) = data.randomSplit([0.6, 0.4], 123)
 
 train.show()
 test.show()
